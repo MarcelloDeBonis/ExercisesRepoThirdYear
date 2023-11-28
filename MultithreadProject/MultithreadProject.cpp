00d9b472139f0022c77f3b7ec23a6784aba48bb1
Marcello De Bonis <marcellodebonis.work@gmail.com>
2023-11-28 15:53:15 +0100

    multithread exercise completed

--- MultithreadProject/MultithreadProject.cpp
+++ MultithreadProject/MultithreadProject.cpp
@@ -1,40 +1,50 @@
-#include "./Public/Philosopher.h"
 #include <iostream>
-#include <vector>
+#include <thread>
 #include <mutex>
+#include <vector>
 
-int main() {
-    const int numPhilosophers = 5;
-    std::vector<std::mutex> forks(numPhilosophers);
-    std::vector<Philosopher> philosophers;
-    std::vector<std::string> names = {"Aristotle", "Plato", "Socrates", "Confucius", "Descartes"};
-    std::vector<std::string> meals = {"Sushi", "Pizza", "Pasta", "Salad", "Steak"};
-    std::vector<std::chrono::milliseconds> mealCosts = {
-        std::chrono::milliseconds(30000),
-        std::chrono::milliseconds(10000),
-        std::chrono::milliseconds(15000),
-        std::chrono::milliseconds(8000),
-        std::chrono::milliseconds(20000)
-    };
-    std::vector<std::chrono::milliseconds> thinkingTimes = {
-        std::chrono::milliseconds(1000),
-        std::chrono::milliseconds(1000),
-        std::chrono::milliseconds(5000),
-        std::chrono::milliseconds(5000),
-        std::chrono::milliseconds(5000)
-    };
+using namespace std;
 
-    Philosopher::Initialize(numPhilosophers);
+const int philosopherNum = 10;
 
-    for (int i = 0; i < numPhilosophers; ++i) {
-        philosophers.emplace_back(names[i], meals[i], mealCosts[i], forks[i], forks[(i + 1) % numPhilosophers], thinkingTimes[i]);
+mutex forks[philosopherNum];
+vector<thread> philosophers;
+
+
+void philosophersAction(int philosopherID) {
+
+    int leftFork = philosopherID;
+    int rightFork = (philosopherID + 1) % philosopherNum;
+
+    while (true)
+    {
+        //Thinking
+        cout << "Philosopher " << philosopherID << " is thinking." << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        //Pick up forks
+        forks[leftFork].lock();
+        forks[rightFork].lock();
+
+        //Eating
+        cout << "Philosopher " << philosopherID << " is eating." << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        forks[leftFork].unlock();
+        forks[rightFork].unlock();
+    }
+}
+
+
+int main()
+{
+    for (int i = 0; i < philosopherNum; i++) {
+        philosophers.emplace_back(philosophersAction, i);
     }
 
-    // Wait for all philosophers to finish eating
-    std::unique_lock<std::mutex> lock(Philosopher::GetCounterMutex());
-    Philosopher::GetCondition().wait(lock, Philosopher::AllHaveEaten);
-
-    std::cout << "All philosophers have finished eating." << std::endl;
+    for (auto& philosopher : philosophers) {
+        philosopher.join();
+    }
 
     return 0;
 }
